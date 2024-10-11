181d498d1124667f93c6c4ac38fe665d7c57246c
Jayesh Ahirrao <ahirraojayesh88@gmail.com>
2024-10-11 12:48:43 +0530

    env issue fixed

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Toaster } from 'react-hot-toast';
-
-const CLIENT_ID = "782125243852-vdc6p226ng7rsi7t40vvft8q0g83rh7r.apps.googleusercontent.com";
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={CLIENT_ID || ""}>
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-      />
-      <App />
-    </GoogleOAuthProvider>
+    <Toaster
+      position="top-center"
+      reverseOrder={false}
+    />
+    <App />
   </StrictMode>,
 )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,9 +3,9 @@ import useGoogleLogin from '../hooks/useGoogleLogin';
 
 const Login = () => {
     const { handleSuccess, handleError } = useGoogleLogin();
-
+    const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
     return (
-        <GoogleOAuthProvider clientId="782125243852-vdc6p226ng7rsi7t40vvft8q0g83rh7r.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId={clientId}>
             <div className="flex justify-center items-center h-screen">
                 <GoogleLogin
                     onSuccess={handleSuccess}
